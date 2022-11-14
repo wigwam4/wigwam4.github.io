4a86f0ec1bd4a4fd26bc2837e71cd38528e37767
Git Test <https://github.com/wigwam4/test.git>
2022-11-14 22:55:31 +0900

    레이아웃 수정
    태그 수정, Skills 내용 변경, Projects 나열 순서 변경

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 (function(){
     const canvasOuter = document.querySelector('.canvas_area .shape_outer');
     const canvasInner = document.querySelector('.canvas_area .shape_inner');
-    const canvasText = document.querySelector('.canvas_area h2');
+    const canvasText = document.querySelector('.canvas_area p');
     const scrollHeight = document.querySelector('.pf_title').scrollHeight;
     let canvasData = {
         outer: {start: 0, end: 0, y1: 0, y2: 0},
@@ -200,17 +200,13 @@
     const myImg = new Image();
     const gra = ctx.createLinearGradient(0,canvas.height/2,canvas.width,canvas.height/2);
 
-    // 그라디언트
     gra.addColorStop(0,'#5BA675');
     gra.addColorStop(1,'#0A46BF');
-
-    // 스타일
     ctx.lineCap = 'round'; 
     ctx.lineWidth = 2;
     ctx.strokeStyle = '#000'; 
-
-    // 실행
     myImg.src = '../img/bgSmile.png'; 
+    
     myImg.onload = function(){
         const myPat = ctx.createPattern(myImg,'repeat');
         
@@ -219,31 +215,31 @@
         ctx.fillStyle = myPat;
         ctx.fill();
 
-        // 눈 - 왼쪽
+        // 눈 L
         ctx.beginPath(); ctx.moveTo(275,190); ctx.bezierCurveTo(275,165,300,165,300,190); ctx.bezierCurveTo(300,215,275,215,275,190); ctx.closePath();
         ctx.fillStyle = '#fff'; ctx.fill();
         ctx.stroke();
 
-        // 눈 - 오른쪽
+        // 눈 R
         ctx.beginPath(); ctx.moveTo(300,190); ctx.bezierCurveTo(300,165,325,165,325,190); ctx.bezierCurveTo(325,215,300,215,300,190); ctx.closePath();
         ctx.fillStyle = '#fff'; ctx.fill();
         ctx.stroke();
 
-        // 눈동자 - 왼쪽
+        // 눈동자 L
         ctx.beginPath(); ctx.moveTo(287,192); ctx.bezierCurveTo(287,180,300,180,300,192); ctx.bezierCurveTo(300,204,287,204,287,192); ctx.closePath();
         ctx.fillStyle = '#000'; ctx.fill();
 
-        // 눈동자 - 오른쪽
+        // 눈동자 R
         ctx.beginPath(); ctx.moveTo(306 ,192); ctx.bezierCurveTo(306,180,319,180,319,192); ctx.bezierCurveTo(319,204,306,204,306,192); ctx.closePath();
         ctx.fillStyle = '#000'; ctx.fill();
 
-        // 눈커플 - 왼쪽
+        // 눈커플 L
         ctx.beginPath(); ctx.moveTo(275,194); ctx.bezierCurveTo(273,164,302,164
         ,300,188); ctx.closePath();
         ctx.fillStyle = '#fff'; ctx.fill();
         ctx.stroke();
 
-        // 눈커플 - 오른쪽
+        // 눈커플 R
         ctx.beginPath(); ctx.moveTo(300,190); ctx.bezierCurveTo(300,165,325,165,325,190); ctx.closePath();
         ctx.fillStyle = '#fff'; ctx.fill();
         ctx.stroke();
@@ -284,69 +280,4 @@
         ctx.moveTo(50,300); ctx.arcTo(100,300,100,230,25); ctx.lineTo(100,250);
         ctx.stroke();
     }
-})();
-
-
-// 세포 그리기
-(function(){
-    const canvas = document.getElementById('canvas_dots');
-    const ctx = canvas.getContext('2d');
-    const color = [
-        {fillstyle: 'rgba(48,161,147,0.6)', strokestyle: 'rgba(48,161,147,0.2)'},
-        {fillstyle: 'rgba(9,140,158,0.6)', strokestyle: 'rgba(9,140,158,0.2)'},
-        {fillstyle: 'rgba(48,99,144,0.6)', strokestyle: 'rgba(48,99,144,0.2)'}
-    ]
-    let particleArray = [];
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    window.onresize = function(){
-        canvas.width = innerWidth;
-        canvas.height = innerHeight;
-    }
-
-    function Particle(x,y,i){
-        this.x = x;
-        this.y = y;
-        this.vol = {
-            x: Math.random()*1.5,
-            y: Math.random()*1.5
-        }
-        this.radius = Math.random()*35+15; // 15~36 중 랜덤 숫자
-        this.draw = () => {  
-            ctx.beginPath();
-            ctx.lineWidth = 28;
-            ctx.fillStyle = color[i].fillstyle;
-            ctx.strokeStyle = color[i].strokestyle;
-            ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
-            ctx.fill(); ctx.stroke();
-        }
-        this.update = () => {
-            this.x += this.vol.x;
-            this.y += this.vol.y;
-            if (this.x - this.radius < 10 || this.x + this.radius > canvas.width){
-                this.vol.x = -this.vol.x;
-            }
-            if (this.y - this.radius < 10 || this.y + this.radius > canvas.height){
-                this.vol.y = -this.vol.y;
-            }
-            this.draw();
-        }
-        this.update();
-    }
-    function animate(){
-        requestAnimationFrame(animate);
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        for (let i=0; i<particleArray.length; i++){
-            particleArray[i].update(); // = new Particle(x,y,colorIndex).update()
-        }
-    }
-
-    for (let i=0; i<40; i++){
-        const x = Math.random() * canvas.width;
-        const y = Math.random() * canvas.height;
-        const colorIndex = Math.floor(Math.random()*color.length);
-        particleArray.push(new Particle(x,y,colorIndex));
-    }
-    animate();
 })();
